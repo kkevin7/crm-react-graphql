60f2a069f310d4adf20c0262759517b16af84e1f
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-11-01 11:40:44 -0600

    Pagination now is complete

--- cliente/src/components/clientes/Clientes.js
+++ cliente/src/components/clientes/Clientes.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, {Component, Fragment } from "react";
 import { Query, Mutation } from "react-apollo";
 import { Link } from "react-router-dom";
 
@@ -6,58 +6,103 @@ import { Link } from "react-router-dom";
 import { CLIENTES_QUERY } from "../../queries/index";
 import { ELIMINAR_CLIENTE } from "../../mutations";
 
-const Contactos = () => (
-  <Query query={CLIENTES_QUERY} pollInterval={1000}>
-    {({ loading, error, data, startPolling, stopPolling }) => {
-      if (loading) return "Cargando...";
-      if (error) error`Èrror: ${error.message}`;
-      console.log(data.getClientes);
-      return (
-        <Fragment>
-          <h2 className="text-center">Listado Clientes</h2>
-          <ul className="list-group mt-4">
-            {data.getClientes.map(item => {
-            const {id} = item;
-            
-            return (
-              <li key={item.id} className="list-group-item">
-                <div className="row justify-content-between align-items-center">
-                  <div className="col-md-8 d-flex justify-content-between align-items-center">
-                    {item.nombre} {item.apellido} - {item.empresa}
-                  </div>
-                  <div className="col-md-4 d-flex justify-content-end">
-                    <Mutation mutation={ELIMINAR_CLIENTE}>
-                      {eliminarCliente => (
-                        <button
-                          type="button"
-                          className="btn btn-danger d-block d-md-inline-block mr-2"
-                          onClick={() => {
-                            if(window.confirm('¿Seguro que deseas eliminar este cliente?')){
-                              eliminarCliente({
-                                variables: {id}
-                              })
-                            }
-                          }}
-                        >
-                          &times; Elimibar
-                        </button>
-                      )}
-                    </Mutation>
-                    <Link
-                      to={`/cliente/editar/${item.id}`}
-                      className="btn btn-success d-block d-md-inline-block"
-                    >
-                      Editar Cliente
-                    </Link>
-                  </div>
-                </div>
-              </li>
-            )})}
-          </ul>
-        </Fragment>
-      );
-    }}
-  </Query>
-);
+import Paginador from './Paginador';
 
-export default Contactos;
+class Clientes extends Component {
+
+  limite = 5;
+
+  state = {
+    paginador: {
+      offset: 0,
+      actual: 1
+    }
+  }
+
+  paginaAnterior = () =>{
+    this.setState({
+      paginador: {
+        offset : this.state.paginador.offset -this.limite,
+        actual: this.state.paginador.actual -1
+      }
+    })
+}
+paginaSiguiente = () => {
+    this.setState({
+      paginador: {
+        offset : this.state.paginador.offset +this.limite,
+        actual: this.state.paginador.actual +1
+      }
+    })
+}
+
+  render() {
+    return (
+      <Query query={CLIENTES_QUERY} pollInterval={1000} variables={{limite: this.limite, offset: this.state.paginador.offset}}>
+        {({ loading, error, data, startPolling, stopPolling }) => {
+          if (loading) return "Cargando...";
+          if (error) error`Error: ${error.message}`;
+          console.log(data);
+          return (
+            <Fragment>
+              <h2 className="text-center">Listado Clientes</h2>
+              <ul className="list-group mt-4">
+                {data.getClientes.map(item => {
+                  const { id } = item;
+
+                  return (
+                    <li key={item.id} className="list-group-item">
+                      <div className="row justify-content-between align-items-center">
+                        <div className="col-md-8 d-flex justify-content-between align-items-center">
+                          {item.nombre} {item.apellido} - {item.empresa}
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-end">
+                          <Mutation mutation={ELIMINAR_CLIENTE}>
+                            {eliminarCliente => (
+                              <button
+                                type="button"
+                                className="btn btn-danger d-block d-md-inline-block mr-2"
+                                onClick={() => {
+                                  if (
+                                    window.confirm(
+                                      "¿Seguro que deseas eliminar este cliente?"
+                                    )
+                                  ) {
+                                    eliminarCliente({
+                                      variables: { id }
+                                    });
+                                  }
+                                }}
+                              >
+                                &times; Elimibar
+                              </button>
+                            )}
+                          </Mutation>
+                          <Link
+                            to={`/cliente/editar/${item.id}`}
+                            className="btn btn-success d-block d-md-inline-block"
+                          >
+                            Editar Cliente
+                          </Link>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+              <Paginador
+                actual={this.state.paginador.actual}
+                totalClientes={data.totalClientes}
+                limite={this.limite}
+                paginaAnterior={this.paginaAnterior}
+                paginaSiguiente={this.paginaSiguiente}
+              />
+            </Fragment>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Clientes;
