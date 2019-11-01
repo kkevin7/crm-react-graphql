443dae8238cdca44b95a6664ecea18a50fc0c651
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-11-01 05:17:39 -0600

    Show record in the Edit form

--- cliente/src/components/clientes/EditarCliente.js
+++ cliente/src/components/clientes/EditarCliente.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { CLIENTE_QUERY } from '../../queries';
+import {Query} from 'react-apollo';
+import FormularioEditarCliente from './FormularioEditarCliente';
 
 class EditarCliente extends Component {
     constructor(props) {
@@ -6,8 +9,28 @@ class EditarCliente extends Component {
         this.state = {  };
     }
     render() {
+        // tomar el ID del contacto a editar
+        const {id} = this.props.match.params;
+        console.log(id);
         return (
-            <h2 className="text-center">EditarCliente</h2>
+            <Fragment>
+                <h2 className="text-center">EditarCliente</h2>
+            <div className="row justify-content-center">
+            <Query query={CLIENTE_QUERY} variables={{id}}>
+                {({loading, error, data}) => {
+                    if(loading) return 'Cargando...';
+                    if(error) return `Error! ${error.message}`;
+                    console.log(data);
+
+                    return(
+                        <FormularioEditarCliente 
+                            cliente={data.getCliente}
+                        />
+                    )
+                }}
+            </Query>
+            </div>
+            </Fragment>
         );
     }
 }
