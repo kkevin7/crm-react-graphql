3d9d5c527198dec7bc97b1dc14d2a56df0e884c3
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-11-02 08:53:43 -0600

    Queries and Mutations is funcional on the server

--- graphql-server/data/db.js
+++ graphql-server/data/db.js
@@ -13,6 +13,8 @@ mongoose.connect('mongodb://localhost:27017/clientes', {
 //   useNewUrlParser: true
 // }).catch(error => console.log(error));
 
+mongoose.set('setFindAndModify', false);
+
 const clientesSchema = new mongoose.Schema({
     nombre: String,
     apellido: String,
@@ -21,8 +23,15 @@ const clientesSchema = new mongoose.Schema({
     edad: Number,
     tipo: String,
     pedidos: Array
-})
+});
 
 const Clientes = mongoose.model('clientes', clientesSchema);
 
-export {Clientes}
+const productosSchema = new mongoose.Schema({
+  nombre: String,
+  precio: Number,
+  stock: Number
+});
+const Productos = mongoose.model('productos', productosSchema);
+
+export {Clientes, Productos}
--- graphql-server/data/resolvers.js
+++ graphql-server/data/resolvers.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { Clientes } from "./db";
+import { Clientes, Productos } from "./db";
 import { rejects } from 'assert';
 
 export const resolvers = {
@@ -20,6 +20,17 @@ export const resolvers = {
         Clientes.countDocuments({}, (error, count) => {
           if(error) rejects(error);
           else resolve(count);
+        })
+      })
+    },
+    obtenerProductos: (root, {limite, offset}) => {
+      return Productos.find({}).limit(limite).skip(offset);
+    },
+    obtenerProducto: (root, {id}) => {
+      return new Promise((resolve, object) => {
+        Productos.findById(id, (error, producto) => {
+          if(error) rejects(error)
+          else resolve(producto)
         })
       })
     }
@@ -60,6 +71,37 @@ export const resolvers = {
           else resolve("Se eliminó correctamente");
         })
       });
+    },
+    nuevoProducto: (root, {input}) => {
+      const nuevoProducto = new Productos({
+        nombre: input.nombre,
+        precio: input.precio,
+        stock: input.stock
+      });
+      //mongo db crea el ID que se asigna al objeto
+      nuevoProducto.id = nuevoProducto._id;
+      return new Promise((resolve, object) => {
+        nuevoProducto.save((error) => {
+          if(error) rejects(error)
+          else resolve(nuevoProducto)
+        })
+      });
+    },
+    actualizarProducto: (root, {input}) => {
+      return new Promise((resolve, producto) => {
+        Productos.findOneAndUpdate({_id: input.id}, input, {new: true}, (error, producto) => {
+          if(error) rejects(error);
+          else resolve(producto);
+        }) 
+      })
+    },
+    eliminarProducto: (root, {id}) => {
+      return new Promise((resolve, producto) => {
+        Productos.findOneAndDelete({_id: id}, (error) => {
+          if(error) rejects(error);
+          else resolve("Se eliminó correctamente");
+        })
+      })
     }
   }
 };
